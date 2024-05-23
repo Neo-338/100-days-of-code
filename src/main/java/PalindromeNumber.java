366d2bcbbd6ecadbb42bffde87df49821dbad5ce
Neo-338 <111322781+Neo-338@users.noreply.github.com>
2024-05-22 20:10:50 -0500

    Actualizacion hasta el dia 6 #100DaysOfCode'

---
+++ src/main/java/PalindromeNumber.java
@@ -0,0 +1,27 @@
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        int Number = 13031;
+        System.out.println(isPalindrome(Number));
+
+    }
+    public static boolean isPalindrome(int originalNumber){
+
+        int tempResult = originalNumber;
+        int reversedNumber = 0;
+
+        if (originalNumber < 0) {
+            return false ;
+        } else {
+            while (tempResult > 0) {
+                int remainder = tempResult % 10;
+                reversedNumber = reversedNumber * 10 + remainder;
+                tempResult = tempResult / 10;
+            }
+            if (originalNumber == reversedNumber) {
+                return true ;
+            } else {
+                return false ;
+            }
+        }
+    }
+}
